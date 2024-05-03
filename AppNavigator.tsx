eb499b91b62ae994d5f1a5871ce65a1a94a3d3b3
JUNSEOK <kd02109@gmail.com>
2024-05-03 10:40:38 +0900

    fix :interseoptor 구조 수정

--- AppNavigator.tsx
+++ AppNavigator.tsx
@@ -8,14 +8,12 @@ import React, {useEffect} from 'react';
 import SignIn from '@page/SignIn';
 import SignUp from '@page/SignUp';
 import {useAppDispatch, useAppSelector} from '@/store';
-import {instance, postWithRefreshToken} from '@/api';
+import {postWithRefreshToken} from '@/api';
 import {Alert} from 'react-native';
 import {UserState, userSlice} from '@/slice/user';
 import useSocket from '@/hook/useSocket';
 import {orderSlice} from '@/slice/order';
 import {Order} from '@/type';
-import {AxiosResponse} from 'axios';
-import {isAxiosError} from 'axios';
 
 export type LoggedInParamList = {
   Orders: undefined;
@@ -35,41 +33,6 @@ export default function AppNavigator() {
   const isLoggedIn = useAppSelector(state => !!state.user.email);
   const dispatch = useAppDispatch();
   const [socket, disconnect] = useSocket();
-
-  // 토큰 갱신
-  useEffect(() => {
-    const callbackSucess = (response: AxiosResponse<any, any>) => response;
-
-    const useCallbackError = async (error: any) => {
-      if (isAxiosError(error)) {
-        // 토큰이 만료된 경우 토큰 갱신
-        // 본래 요청에 대한 정보는 error.config에 담겨져 있습니다.
-        const {response, config} = error;
-        const message: string = response!.data.message;
-
-        if (
-          response?.status === 419 &&
-          response?.data.code === 'expired' &&
-          message.includes('액세스')
-        ) {
-          const originalRequest = config!;
-          const data = await postWithRefreshToken<UserState | string>(
-            '/refreshToken',
-          );
-          if (typeof data === 'object') {
-            // 새로운 토큰 저장 및 본래 요청 실행
-            dispatch(userSlice.actions.setAccesToken(data.accessToken));
-            originalRequest.headers.Authorization = `Bearer ${data.accessToken}`;
-            return instance(originalRequest);
-          }
-        }
-        // 리프레시 토큰 만료
-        return Promise.reject(error);
-      }
-      return Promise.reject(error);
-    };
-    instance.interceptors.response.use(callbackSucess, useCallbackError);
-  }, [dispatch]);
 
   // 토큰 재발급
   useEffect(() => {
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,10 @@
 import {REFRESH_TOKEN} from '@/lib/constant';
 import {getStorage} from '@/lib/encryptedStorage';
 import {SignInSchema, SignUpSchema} from '@/schema/schma';
-import {UserState} from '@/slice/user';
+import {UserState, userSlice} from '@/slice/user';
+import {store} from '@/store';
 
-import axios, {AxiosError, AxiosResponse} from 'axios';
+import axios, {AxiosError, AxiosResponse, isAxiosError} from 'axios';
 
 import Config from 'react-native-config';
 
@@ -15,6 +16,40 @@ export const instance = axios.create({
 });
 
 instance.interceptors.request.use();
+
+const callbackSucess = (response: AxiosResponse<any, any>) => response;
+
+const useCallbackError = async (error: any) => {
+  if (isAxiosError(error)) {
+    // 토큰이 만료된 경우 토큰 갱신
+    // 본래 요청에 대한 정보는 error.config에 담겨져 있습니다.
+    const {response, config} = error;
+    const message: string = response!.data.message;
+
+    if (
+      response?.status === 419 &&
+      response?.data.code === 'expired' &&
+      message.includes('액세스')
+    ) {
+      const originalRequest = config!;
+      const data = await postWithRefreshToken<UserState | string>(
+        '/refreshToken',
+      );
+      if (typeof data === 'object') {
+        // 새로운 토큰 저장 및 본래 요청 실행
+        //dispatch(userSlice.actions.setAccesToken(data.accessToken));
+        originalRequest.headers.Authorization = `Bearer ${data.accessToken}`;
+        store.dispatch(userSlice.actions.setAccesToken(data.accessToken));
+        return instance(originalRequest);
+      }
+    }
+    // 리프레시 토큰 만료
+    return Promise.reject(error);
+  }
+  return Promise.reject(error);
+};
+
+instance.interceptors.response.use(callbackSucess, useCallbackError);
 
 export async function postSignup(data: SignUpSchema) {
   let message = '';
