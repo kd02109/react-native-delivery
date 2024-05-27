8ea6d29a7c4a69de827fa6e17ecd24366649c22b
JUNSEOK <kd02109@gmail.com>
2024-05-27 20:03:37 +0900

    feat : react native sentry 연결

--- App.tsx
+++ App.tsx
@@ -2,6 +2,19 @@ import * as React from 'react';
 
 import ReduxProvider from '@/components/ReduxProvider';
 import AppNavigator from 'AppNavigator';
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://8428cf35f98b031523764b6f2a11f108@o4507110752911360.ingest.us.sentry.io/4507327827148800',
+  // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+  // We recommend adjusting this value in production.
+  tracesSampleRate: 1.0,
+  _experiments: {
+    // profilesSampleRate is relative to tracesSampleRate.
+    // Here, we'll capture profiles for 100% of transactions.
+    profilesSampleRate: 1.0,
+  },
+});
 
 function App() {
   return (
@@ -11,4 +24,4 @@ function App() {
   );
 }
 
-export default App;
+export default Sentry.wrap(App);
